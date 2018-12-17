01f055aeb4c1e85f95c8c48a0e2903a15aa06829
daryushkari <daryushkari123@gmail.com>
2018-12-18 00:49:29 +0330

    make views more readable

--- app/views.py
+++ app/views.py
@@ -5,27 +5,12 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 import templates.front
 from . import forms
-
+from .models import Applicant
 # Create your views here.
 
 
 def hello(request):
     return render(request, 'front/index.html')
-
-
-def corporate(request):
-    if request.method == "POST":
-
-        form = forms.ApplicantSignIn(request.POST)
-
-        if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            email = form.cleaned_data['email']
-            return HttpResponse(str(email)+str(first_name))
-    else:
-        form = forms.ApplicantSignIn(request.POST)
-        print("get request")
-    return render(request, 'front/corporate_templates/corporate_sign_in.html', {'form': form})
 
 
 def applicant(request):
@@ -41,3 +26,18 @@ def applicant(request):
         form = forms.ApplicantSignIn(request.POST)
         print("get request")
     return render(request, 'front/applicant_templates/applicant_sign_in.html', {'form': form})
+
+
+def corporate(request):
+    if request.method == "POST":
+
+        form = forms.ApplicantSignIn(request.POST)
+
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            email = form.cleaned_data['email']
+            return HttpResponse(str(email)+str(first_name))
+    else:
+        form = forms.ApplicantSignIn(request.POST)
+        print("get request")
+    return render(request, 'front/corporate_templates/corporate_sign_in.html', {'form': form})
